e58917428bd12f053b569a7811c38586a9297398
Jerome Joof <jeromejoof@yahoo.com>
2020-01-15 22:48:32 -0800

    fixed the gradle and tests are passing now

---
+++ datastructures/src/main/java/datastructures/LinkedList/Node.java
@@ -0,0 +1,12 @@
+package datastructures.LinkedList;
+
+public class Node {
+    Object newData;
+    Object data;
+    Node next;
+
+    public Node(Object d, Node next) {
+        this.data = d;
+        this.next = null;
+    }
+}
