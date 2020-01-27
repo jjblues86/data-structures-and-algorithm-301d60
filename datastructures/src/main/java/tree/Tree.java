621e27e1c231ff3c5bc19aa857a93e0520213a93
Jerome Joof <jeromejoof@yahoo.com>
2020-01-27 11:52:13 -0800

    all features implemented

---
+++ datastructures/src/main/java/tree/Tree.java
@@ -0,0 +1,74 @@
+package tree;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Tree<E> {
+    Node root;
+
+    public Tree() {
+        this.root = null;
+    }
+
+
+    //create a default constructor
+    public Tree(Node root){
+        this.root = root;
+    }
+
+    public boolean isEmpty(){
+        return null == root;
+    }
+
+
+    //if new node value is lower than the current node -> go to the left
+    //if new node value is greater than the current node -> go to the right
+    //if the current node is null -> a leaf node exist and therefore insert new node in that position
+    //depth-first traversal
+
+    //pre-order: root -> left -> right
+    public void traversePreOrder(Node node){
+        if(node != null){
+            System.out.println("node = " + node.value);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+//    public List<? extends Object> traversePreOrder(Node<E> node){
+//        if(node != null){
+//            return new ArrayList<E>();
+//        } else {
+//            ArrayList<Integer> left = traverseInOrder(node.left);
+//            ArrayList<E> right = (ArrayList<E>) traverseInOrder(node.right);
+//
+//            ArrayList<Integer> outcome = new ArrayList<>();
+//            outcome.addAll(left);
+//            outcome.add(node.value);
+//            outcome.addAll((Collection<? extends Integer>) right);
+//            return outcome;
+//
+//        }
+//
+//    }
+
+    //post-order: left -> right -> root
+    public void traversePostOrder(Node node){
+        if(node != null){
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.println("node = " + node.value);
+        }
+    }
+
+    //in-order: left -> root -> right
+    public ArrayList<Integer> traverseInOrder(Node node){
+        if(node != null){
+            traverseInOrder(node.left);
+            System.out.println("node = " + node.value);
+            traverseInOrder(node.right);
+        }
+        return null;
+    }
+}
---
+++ datastructures/src/test/java/tree/TreeTest.java
@@ -0,0 +1,57 @@
+package tree;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class TreeTest {
+
+    @Test
+    public void testEmptyTree(){
+        Tree tree = new Tree();
+        assertNull(tree.root);
+    }
+
+    @Test
+    public void testInsertBinarySearchTree(){
+        //      7
+        //   5     9
+        //4
+        Node root = new Node(7, new Node(5, new Node(4), null), new Node(9));
+        Tree tree = new Tree(root);
+        assertEquals(7, tree.root.value);
+        assertEquals(5, tree.root.left.value);
+    }
+    @Test
+    public void testBinaryTreeIsEmpty(){
+        Tree tree = new Tree();
+        assertTrue(tree.isEmpty());
+    }
+    @Test
+    public void testBinarySearchTreeIsEmpty_withNodes(){
+        // 7
+        //5 9
+        Node root = new Node(7, new Node(5), new Node(9));
+        Tree tree = new Tree(root);
+        assertFalse(tree.isEmpty());
+    }
+    @Test
+    public void testBinaryTreeTraverseInorder(){
+        //      7
+        //  5      9
+        //4   6  8
+        Node root = new Node(7, new Node(5, new Node(4), new Node(6)), new Node(9), new Node(8), null);
+        Tree tree = new Tree(root);
+        tree.traverseInOrder(tree.root);
+//        assertEquals((int)tree.traverseInOrder(root.value));
+//        ArrayList<Integer> trees = new ArrayList<>(Arrays.asList(4,5,6,7,8,9));
+//        assertEquals(trees);
+    }
+
+
+
+}
